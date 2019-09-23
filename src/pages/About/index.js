3eedbfa241695ab143ebc5bc5597f3ef0db46321
robson maia <robsonmvieira@gmail.com>
2019-09-23 16:52:00 -0300

    :pencil: Added about description in About screen

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -6,7 +6,26 @@ import {View, Text} from 'react-native'
 export default function About() {
   return (
     <View>
-      <Text>About Screen</Text>
+      <Text>Who i'm ?</Text>
+      <Text>
+        Hello! I'm Robson Maia. I'm Javascript and Ruby fullstack developer
+      </Text>
+      <Text>
+        I'm Graduated in Analitic and development of system from INSTITUTO
+        INFNET in Rio de Janeiro - Brazil
+      </Text>
+      <Text>My Passion</Text>
+      <Text>I Love technology and as it can change our lives.</Text>
+      <Text>
+        I'm fluent in reactJS, react-nativeJS, nodeJs, relational database like
+        this MySql or PostgreSql. Also developement using Ruby and your
+        principal framework Rails.
+      </Text>
+      <Text>I love football and i'm Barcelona's fun.</Text>
+      <Text>
+        In my days-off i been studing english classes for improve them.
+      </Text>
+      <Text>If you want known more about me let me known</Text>
     </View>
   )
 }
